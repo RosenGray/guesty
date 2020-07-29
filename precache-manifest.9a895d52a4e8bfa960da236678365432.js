666cf0206b0b6e6e927c43a6f99325c7efd20220
Vladi Iokhim <mac@macs-MacBook-Pro.local>
2020-07-29 11:49:41 +0300

    Updates

---
+++ precache-manifest.9a895d52a4e8bfa960da236678365432.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d2e6d1195acd6a764f13e7afb608bd5",
+    "url": "/guesty/index.html"
+  },
+  {
+    "revision": "95859d7d3bd1b26b8c8c",
+    "url": "/guesty/static/css/main.64b54e3b.chunk.css"
+  },
+  {
+    "revision": "c8359501f730bbcebbd0",
+    "url": "/guesty/static/js/2.2e4d2611.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/guesty/static/js/2.2e4d2611.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95859d7d3bd1b26b8c8c",
+    "url": "/guesty/static/js/main.3f51f8c3.chunk.js"
+  },
+  {
+    "revision": "f8326a9613980f17e6b7",
+    "url": "/guesty/static/js/runtime-main.a7c3d39f.js"
+  }
+]);
